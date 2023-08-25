c40c3b914c85f088dc100ba7fd00fdd4600d1507
dephillips98 <dephillips5262@gmail.com>
2023-08-25 21:06:26 +0000

    adding input route

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+#!/usr/bin/env python3
 from flask import Flask
-
+from flask import requests
+from flask import render_template
+from flask import redirect
 app = Flask(__name__)
 
 @app.route("/")
 def login():
     return "<p>Hello Team</p>"
+#dp
+@app.route("/input")
+def user_input():
+
+#dp
