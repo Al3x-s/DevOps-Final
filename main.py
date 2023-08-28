8086f06e9f052f1d836763f7766bad3b4e29fb6d
dephillips98 <dephillips5262@gmail.com>
2023-08-28 14:30:25 +0000

    working on input route in main.py

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ app = Flask(__name__)
 def login():
     return "<p>Hello Team</p>"
 #dp
-@app.route("/input")
+@app.route("/input", methods=["POST"])
 def user_input():
-
+#creating section for user input
+    if request.form("Full Name"):
+        name = request.form("Full Name")
+        print(name)
+if __name__ == __main__
+    app.run(host="0.0.0.0", port=2225, debug=True)
+    
 #dp
