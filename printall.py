736255193d51bd64bf21b84139c8244258e08678
Alejandro Sanchez <33365377+Al3x-s@users.noreply.github.com>
2023-08-29 10:41:22 -0500

    Delete printall.py

--- printall.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-def printall():
-    print("========================================")
-    conn = sqlite3.connect("user.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users")
-    print(cursor.fetchall())
-    conn.close
-printall()
